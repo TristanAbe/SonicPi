55ccdcf15202b79d331c31882293f408f9ac9b90
TristanAbraham <99356268+TristanAbraham@users.noreply.github.com>
2022-02-14 11:40:49 -0800

    Update ChallengeC.rb

--- ChallengeC.rb
+++ ChallengeC.rb
@@ -1,1 +1,36 @@
 
+use_bpm 160
+use_synth :saw
+
+define :stranger_things_notes do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+
+
+with_fx :ping_pong do
+  stranger_things_notes
+end
+
+with_fx :autotuner do
+  stranger_things_notes
+end
+with_fx :compressor do
+  stranger_things_notes
+end
+
